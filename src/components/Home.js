38cd66df97d6c8da1016dd7dc00e89745606922a
Rayan Bouezzeddine <113249440+Rayan200013@users.noreply.github.com>
2023-08-19 15:19:45 +0300

    github update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "@mui/material";
+// import { Link } from "@mui/material";
 import OwlCarousel from "react-owl-carousel"; // Import the React wrapper for Owl Carousel
-import "owl.carousel/dist/assets/owl.carousel.css"; // Import Owl Carousel CSS
 import "owl.carousel/dist/assets/owl.theme.default.css"; // Import Owl Carousel default theme CSS
-import "./GameSection.css"; // Import your custom CSS if needed
+import "../App.css"; // Import your custom CSS if needed
 
 const GameSection = () => {
   const [activeItem, setActiveItem] = useState(0);
@@ -35,7 +34,71 @@ const GameSection = () => {
             <p>Dota 2 is a multiplayer online battle arena by Valve...</p>
           </div>
         </div>
-        {/* ... Repeat for other items */}
+        <div
+          className={`item ${activeItem === 0 ? "active" : ""}`}
+          style={{
+            backgroundImage:
+              "url(https://ucarecdn.com/75d7700d-c102-40ff-9ba1-f0641444c616/dota2.jpg)",
+          }}
+          onClick={() => handleItemClick(0)}
+        >
+          <div className="item-desc">
+            <h3>Dota 2</h3>
+            <p>Dota 2 is a multiplayer online battle arena by Valve...</p>
+          </div>
+        </div>
+        <div
+          className={`item ${activeItem === 0 ? "active" : ""}`}
+          style={{
+            backgroundImage:
+              "url(https://ucarecdn.com/75d7700d-c102-40ff-9ba1-f0641444c616/dota2.jpg)",
+          }}
+          onClick={() => handleItemClick(0)}
+        >
+          <div className="item-desc">
+            <h3>Dota 2</h3>
+            <p>Dota 2 is a multiplayer online battle arena by Valve...</p>
+          </div>
+        </div>
+        <div
+          className={`item ${activeItem === 0 ? "active" : ""}`}
+          style={{
+            backgroundImage:
+              "url(https://ucarecdn.com/75d7700d-c102-40ff-9ba1-f0641444c616/dota2.jpg)",
+          }}
+          onClick={() => handleItemClick(0)}
+        >
+          <div className="item-desc">
+            <h3>Dota 2</h3>
+            <p>Dota 2 is a multiplayer online battle arena by Valve...</p>
+          </div>
+        </div>
+        <div
+          className={`item ${activeItem === 0 ? "active" : ""}`}
+          style={{
+            backgroundImage:
+              "url(https://ucarecdn.com/75d7700d-c102-40ff-9ba1-f0641444c616/dota2.jpg)",
+          }}
+          onClick={() => handleItemClick(0)}
+        >
+          <div className="item-desc">
+            <h3>Dota 2</h3>
+            <p>Dota 2 is a multiplayer online battle arena by Valve...</p>
+          </div>
+        </div>
+        <div
+          className={`item ${activeItem === 0 ? "active" : ""}`}
+          style={{
+            backgroundImage:
+              "url(https://ucarecdn.com/75d7700d-c102-40ff-9ba1-f0641444c616/dota2.jpg)",
+          }}
+          onClick={() => handleItemClick(0)}
+        >
+          <div className="item-desc">
+            <h3>Dota 2</h3>
+            <p>Dota 2 is a multiplayer online battle arena by Valve...</p>
+          </div>
+        </div>
       </OwlCarousel>
     </section>
   );
