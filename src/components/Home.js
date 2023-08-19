eed9f17c21758c8c819a45994d48fbcc69513771
Rayan Bouezzeddine <113249440+Rayan200013@users.noreply.github.com>
2023-08-19 15:15:33 +0300

    new commit categories

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,44 @@
-const Home = () => {
+import React, { useState, useEffect } from "react";
+import { Link } from "@mui/material";
+import OwlCarousel from "react-owl-carousel"; // Import the React wrapper for Owl Carousel
+import "owl.carousel/dist/assets/owl.carousel.css"; // Import Owl Carousel CSS
+import "owl.carousel/dist/assets/owl.theme.default.css"; // Import Owl Carousel default theme CSS
+import "./GameSection.css"; // Import your custom CSS if needed
+
+const GameSection = () => {
+  const [activeItem, setActiveItem] = useState(0);
+
+  const handleItemClick = (index) => {
+    setActiveItem(index === activeItem ? null : index);
+  };
+
+  const carouselOptions = {
+    autoWidth: true,
+    loop: true,
+  };
+
   return (
-    <>
-      <h1>This is HOME</h1>
-      <div class="home"></div>
-    </>
+    <section className="game-section">
+      <h2 className="line-title">trending games</h2>
+      <OwlCarousel className="custom-carousel owl-theme" {...carouselOptions}>
+        {/* Your carousel items */}
+        <div
+          className={`item ${activeItem === 0 ? "active" : ""}`}
+          style={{
+            backgroundImage:
+              "url(https://ucarecdn.com/75d7700d-c102-40ff-9ba1-f0641444c616/dota2.jpg)",
+          }}
+          onClick={() => handleItemClick(0)}
+        >
+          <div className="item-desc">
+            <h3>Dota 2</h3>
+            <p>Dota 2 is a multiplayer online battle arena by Valve...</p>
+          </div>
+        </div>
+        {/* ... Repeat for other items */}
+      </OwlCarousel>
+    </section>
   );
 };
 
-export default Home;
+export default GameSection;
